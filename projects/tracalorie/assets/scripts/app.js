c8d9a375e362c5bccc6053ebf79f1b59c5678039
João Bagio <34991522+jbagio@users.noreply.github.com>
2018-06-04 18:06:45 +0100

    tracalorie - add create item functionality

--- projects/tracalorie/assets/scripts/app.js
+++ projects/tracalorie/assets/scripts/app.js
@@ -7,30 +7,46 @@ const ItemController = (function () {
     this.calories = calories;
   };
 
-  // Internal state / data structure
-  const state = {
-    // Some temp initial data
-    items: [
-      {id: 1, name: 'Chickpea curry', calories: 350},
-      {id: 2, name: 'Mashed surprise', calories: 550},
-      {id: 3, name: 'Green smoothie', calories: 220}
-    ],
+  // Internal data structure
+  const data = {
+    items: [],
     currentItem: null,
     totalCalories: 0
   };
 
   // Public methods
   return {
-    getItems: () => state.items,
-    logState: () => state
+    getItems: () => data.items,
+    getTotalCalories: () => data.totalCalories,
+    addItem: (name, calories) => {
+      // Generate an ID. This guarantees enough uniqueness for this case:
+      // https://stackoverflow.com/questions/8012002/create-a-unique-number-with-javascript-time#comment80201464_18048162
+      let id = performance.now().toString().replace('.', 7);
+
+      // Create new item
+      const newItem = new Item(id, name, parseInt(calories));
+
+      // Push to items array
+      data.items.push(newItem);
+
+      // Update total calories
+      data.totalCalories += newItem.calories;
+    },
+    logData: () => data
   };
 }());
 
 // UI module
 const UIController = (function () {
-  // Selectors for elements
+  // Selectors for DOM elements
   const uiSelectors = {
-    itemList: '#item-list'
+    // Using form submit instead of course's button click,
+    // so we can have cleaner validation
+    itemForm: '#item-form',
+    itemNameInp: '#item-name',
+    itemCaloriesInp: '#item-calories',
+    itemsUl: '#item-list',
+    totalCaloriesSpan: '.total-calories'
   };
 
   // Public methods
@@ -38,7 +54,7 @@ const UIController = (function () {
     renderItems: items => {
       let html = '';
 
-      // Prepare items markup
+      // Prepare items markup and add it to ul
       items.forEach(item => {
         html += `
           <li id="item-${item.id}" class="collection-item">
@@ -48,19 +64,67 @@ const UIController = (function () {
         `;
       });
 
-      // Append to items list section
-      document.querySelector(uiSelectors.itemList).innerHTML = html;
+      document.querySelector(uiSelectors.itemsUl).innerHTML = html;
+    },
+    renderTotalCalories: totalCalories => {
+      // Add value to 'Total Calories' span
+      document.querySelector(uiSelectors.totalCaloriesSpan).innerHTML = totalCalories;
+    },
+    getUiSelectors: () => uiSelectors,
+    // Grab and return item input values
+    getItemInput: () => {
+      return {
+        name: document.querySelector(uiSelectors.itemNameInp).value.trim(),
+        calories: document.querySelector(uiSelectors.itemCaloriesInp).value.trim()
+      };
+    },
+    clearInput: () => {
+      document.querySelector(uiSelectors.itemNameInp).value = '';
+      document.querySelector(uiSelectors.itemCaloriesInp).value = '';
     }
   };
 }());
 
 // Main app module
 const App = (function (itemCtrl, uiCtrl) {
+  const registerEventListeners = () => {
+    // Get selectors from UI Controller
+    const uiSelectors = uiCtrl.getUiSelectors();
+
+    // Form submit event listener
+    document.querySelector(uiSelectors.itemForm).addEventListener('submit', addItemSubmit);
+  };
+
+  // Event listeners
+  // Add item button
+  const addItemSubmit = e => {
+    // Get input values from UI Controller
+    const {name, calories} = uiCtrl.getItemInput(); // Obj destructuring
+
+    // Pass to Item Controller for creation
+    itemCtrl.addItem(name, calories);
+
+    // No need to create addItem method on UiController as per the course...
+    // Simply re-fetch and re-render items!
+    uiCtrl.renderItems(itemCtrl.getItems());
+    uiCtrl.renderTotalCalories(itemCtrl.getTotalCalories());
+
+    uiCtrl.clearInput();
+
+    e.preventDefault();
+  };
+
   // Public methods
   return {
     init: () => {
-      const items = itemCtrl.getItems();
-      uiCtrl.renderItems(items);
+      // Register event listeners
+      registerEventListeners();
+
+      // Get items from data and render them
+      uiCtrl.renderItems(itemCtrl.getItems());
+
+      // Get total calories from data and render it
+      uiCtrl.renderTotalCalories(itemCtrl.getTotalCalories());
     }
   };
 }(ItemController, UIController));
